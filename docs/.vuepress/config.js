b2803d3caa0ac7ef7c30e809b924f5d2392e5b01
chencongyin <398696990@qq.com>
2024-08-09 11:06:53 +0800

    feat：版本更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,59 +1,69 @@
-module.exports = {
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from "vuepress";
+import { viteBundler } from "@vuepress/bundler-vite";
+
+export default defineUserConfig({
+  bundler: viteBundler(),
+  lang: "zh-CN",
   title: "MAO MAO CHU XIA",
   description: "我的个人博客",
-  head: [
-    // 注入到当前页面的 HTML <head> 中的标签
-    ["link", { rel: "icon", href: "/logo.jpg" }], // 增加一个自定义的 favicon(网页标签的图标)
-  ],
-  serviceWorker: true, // 是否开启 PWA
-  base: "/", // 这是部署到github相关的配置
-  markdown: {
-    lineNumers: false, // 代码块显示行号
-  },
-  themeConfig: {
+
+  // head: [
+  //   // 注入到当前页面的 HTML <head> 中的标签
+  //   ["link", { rel: "icon", href: "/logo.jpg" }], // 增加一个自定义的 favicon(网页标签的图标)
+  // ],
+  // serviceWorker: true, // 是否开启 PWA
+  // markdown: {
+  //   lineNumers: false, // 代码块显示行号
+  // },
+  theme: defaultTheme({
     logo: "/images/logo.png",
-    nav: [
+    repo: "", // 项目地址
+    lastUpdated: "上次更新", // 开启页面最后更新时间
+    sidebarDepth: 2, // 侧边栏显示深度
+    navbar: [
       { text: "Home", link: "/" },
       {
         text: "学习笔记",
-        items: [
+        prefix: "/note/",
+        children: [
           {
             text: "前端笔记",
-            items: [{ text: "JavaScript", link: "/note/javascript/" }],
+            prefix: 'javascript/',
+            children: ['README.md']
           },
           {
             text: "Nginx",
-            items: [{ text: "nginx入门", link: "/note/nginx/" }],
+            prefix: 'nginx/',
+            children: ['README.md']
           },
         ],
       },
     ],
-    // siderbar: [
-    //   {
-    //     title: "前端笔记",
-    //     collapsable: true, // 可折叠
-    //     path: "/node/",
-    //     sidebarDepth: 1,
-    //     children: ["/note/javascript/javascript-basic.md"],
-    //   },
-    //   {
-    //     title: "nginx学习笔记",
-    //     collapsable: true, // 可折叠
-    //     children: [
-    //       {
-    //         title: "Nginx",
-    //         path: "/note/nginx/nginx入门.md",
-    //       },
-    //     ],
-    //   },
-    // ],
-  },
-
-  configureWebpack: {
-    resolve: {
-      alias: {
-        "@alias": "path/to/some/dir",
-      },
-    },
-  },
-};
+    sidebar: 'heading',
+    // sidebar: {
+    //   "/note/javascript/": [
+    //     {
+    //       title: "前端笔记",
+    //       collapsable: false, // 可折叠
+    //       children: [
+    //         {
+    //           title: "JavaScript",
+    //           link: "/note/javascript/javascript-basic.md",
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       title: "nginx学习笔记",
+    //       collapsable: false, // 可折叠
+    //       children: [
+    //         {
+    //           title: "Nginx",
+    //           link: "/note/nginx/nginx入门.md",
+    //         },
+    //       ],
+    //     },
+    //   ],
+    // },
+  }),
+});
